d742cb362f771a71e5fab0f4a2f4cbafbfa791f5
Saniya Pawankar <saniyapawankar@gmail.com>
2025-08-19 00:25:51 +0530

    New Updates

---
+++ Javascript/Lessons/5.Functions/problems.js
@@ -0,0 +1,92 @@
+
+
+// wap to add two numbers
+
+// function addNumbers(n1,n2,n3){
+//     let sum = n1 + n2 + n3;
+//     return sum;
+// }
+// console.log(addNumbers(20,24,50));
+
+
+/*-----------------------------------------------------------------------------------------------------------------*/
+// wap to find square of a number
+
+// function squareOfNumber(num){
+//     return num*num;
+// }
+// console.log(`Square of number is ${squareOfNumber(6)}`);
+
+
+/*----------------------------------------------------------------------------------------------------*/
+//wap to find factorial of a number
+
+// let factorial = (num1) => {
+//     let fact = 1;
+//     for (let i = 1; i <= num1; i++) {
+//         fact = fact * i;
+//     }
+//     return fact;
+// }
+
+// console.log("Factorial of given number is " + factorial(5));
+
+/*----------------------------------------------------------------------------------------------------------------*/
+
+// wap to reverse a string
+
+// let reverseString = (str) => {
+//     let str2 = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         str2 = str2 + str.charAt(i);
+//     }
+//     return str2;
+// }
+
+// console.log(`String after reversal = ${reverseString("Hello World!")}`);
+
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+// wap to check if a number is prime or not
+
+// function isPrimeNumber(number) {
+//     let isPrime = true;
+//     if (number == 1) {
+//         isPrime = false;
+//     } else {
+//         for (let i = 2; (i * i) <= number; i++) {
+//             if (number % i == 0) {
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(number + " is a Prime Number.");
+//     }else{
+//          console.log(number + " is Not a  Prime Number.");
+//     }
+// }
+
+// isPrimeNumber(3);
+
+/*-----------------------------------------------------------------------------------------------------------------------------*/
+
+// Write a function to find the sum of digits of a number.
+
+// function sumOfDigits(num) {
+//     let sum = 0;
+//     let digit = 0;
+//     for (let i = num; i > 0; i = parseInt(i / 10)) {
+//         digit = i % 10;
+//         sum = sum + digit;
+//     }
+//     return sum;
+// }
+
+// console.log("Sum of digits = "+sumOfDigits(23456));
+
+
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+
+//
---
+++ Javascript/Lessons/5.Functions/script.js
@@ -0,0 +1,93 @@
+// Defination :- A function in Javascript is a reusable block of code designed to perform a specific task which can be called multiple times.
+
+// Ways to create a function / module
+ 
+// 1) Using function keyword
+
+function function_name(args[optional]){
+    //function body
+    // return statement(optional)
+}
+
+// 2) ES6(Arrow / Fat Arrow Functions, Modules, Callbacks)
+
+let function_name = (args[optional]){
+    //function body
+    // return statement(optional)
+}
+
+// Note : Arrow functions are top-order code and are not hoisted.
+// Traditional function declarations are hoisted (can be used before defination).
+
+
+// Calling a function
+
+function_name(args[optional]);
+
+// Types of functions 
+
+// 1) No arguments, no return
+
+function greet(){
+    console.log("Hello!");
+}
+
+// 2) Arguments, but no return
+
+let name = "Ayush";
+
+function greet(name){
+    console.log("Hello "+name);
+}
+
+// 3) No arguments, return value
+
+function getNumber() {
+    return 10;
+}
+
+// 4) Arguments, Return value
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(1,2));
+
+
+// Function Declaration :
+//  A function declaration is simply the statement where you declare the function with its name, parameters, and body.
+// In JavaScript, a declaration automatically acts as a definition, because it includes the body.
+// Function declarations are hoisted → can be called before they are written. 
+
+// Function Declaration
+function greet(name) {
+    return "Hello " + name;
+}
+
+// Calling before declaration (works because of hoisting)
+console.log(greet("Saniya"));  // Output: Hello Saniya
+
+
+// Function Defination 
+
+// A function definition is the actual implementation (body) of the function.
+// It specifies what the function does.
+// In JavaScript, when we write a declaration with a body, it’s both declaration + definition at the same time.
+// Functions can also be defined using function expressions or arrow functions.
+
+// Function Definition (via expression)
+const greet = function(name) {
+    return "Hello " + name;
+};
+
+console.log(greet("Saniya")); // Output: Hello Saniya
+
+// Key Points:- 
+// Function Declaration → Uses function keyword, is hoisted.
+// Function Definition → The actual implementation (can be via declaration, expression, or arrow).
+// In JavaScript, declaration already includes definition (unlike C/C++ where declaration ≠ definition).
+
+
+// Declaration = Tells JS that a function exists (with body in JS).
+// Definition = The code inside that function that executes.
