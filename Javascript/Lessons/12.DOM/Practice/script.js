c038232535cdd74d4fe16bc96d883d1e9daa835d
Saniya Pawankar <saniyapawankar@gmail.com>
2025-09-09 15:52:16 +0530

    New Changes

--- Javascript/Lessons/12.DOM/Practice/script.js
+++ Javascript/Lessons/12.DOM/Practice/script.js
@@ -4,21 +4,31 @@ let targetElement = document.getElementById("box1");
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", (event) => {
-    if(targetElement.classList.value === "show" ){
+    if (targetElement.classList.value === "show") {
         targetElement.classList.value = "hide";
-    }else{
+    } else {
         targetElement.classList.value = "show";
     }
 })
 
 
 btn.addEventListener("click", (event) => {
-    if (targetElement.classList.contains("show")) {
+    if (targetElement.classList.value === "show") {
         targetElement.classList.value = "";
         targetElement.classList.value += "hide";
     }
     else {
         targetElement.classList.value = "";
         targetElement.classList.value += "show";
+    }
+});
+
+console.log(targetElement.className);
+
+btn.addEventListener("click", () => {
+    if (targetElement.className.includes("show")) {
+        targetElement.className = targetElement.className.replace("show", "hide");
+    } else {
+        targetElement.className = targetElement.className.replace("hide", "show");
     }
 });
