b5263a7720767e175d55b73e287ac87318b33e86
Saniya Pawankar <saniyapawankar@gmail.com>
2025-11-13 14:45:53 +0530

    React

--- React/react2/src/App.jsx
+++ React/react2/src/App.jsx
@@ -1,9 +1,12 @@
-
+import Header from "./components/Header";
+import Footer  from "./components/Footer";
 
 function App() {
-
   return (
-    <div>Hello World!</div>
+    <>
+     <Header/>
+     <Footer/>
+    </>
   )
 }
 
---
+++ React/react2/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+
+    let styleObject={
+         color: "pink",
+         backgroundColor: "blue"
+    }
+    return(
+        <><h2 style={styleObject}>Hi, this is Footer</h2></>
+    )
+}
+
+export default Footer;
